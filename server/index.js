4099b73d38f29954fee69396f6e2989d81e2488f
projectlac <dft1711198@gmail.com>
2021-11-01 17:30:34 +0700

    hahaha

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app.use(cors())
 const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts)
+//Handle Production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public/'))
+
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'));
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Start ${port}`))
